787549beca9b0c32043bf828f0e70a6355bf86ae
IsNotMyIP <SirSeanHD@gmail.com>
2014-04-03 12:19:11 +0200

    Fixed gulpfile for each user

--- gulpfile.js
+++ gulpfile.js
@@ -67,7 +67,7 @@ gulp.task('connect', connect.server({
   port: 9000,
   livereload: true,
   open: {
-    browser: 'Chromium' // if not working on OSX try: 'Google Chrome'
+    browser: 'firefox' // if not working on OSX try: 'Google Chrome'
   }
 }));
 
