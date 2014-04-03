6cbc3619f4219db37710d436a5b9fb0e25cc80f7
IsNotMyIP <SirSeanHD@gmail.com>
2014-04-03 12:58:23 +0200

    Fixed the function Element on a proper site

--- src/js/game.js
+++ src/js/game.js
@@ -1,29 +1,38 @@
 (function() {
   'use strict';
-
+  function Elemento (parent) {
+      this.parent = parent;
+      this.nombre = this.parent.elementName[Math.floor(Math.random() * 80)];
+      this.getType = function() {
+        console.log(this.nombre);
+      };
+    }
   function Game() {
     this.elementName = null;
   }
 
   Game.prototype = {
+    that : this,
 
     create: function () {
-
+      
       this.elementName = ['H','Li','Na','K','Rb','Ce','Fr','Be','Mg','Ca','Sr','Ba','Ra','Sc','Y','Ti','Zr','Hf','Rf','V','Nb','Ta','Db','Cr','Mo','W','Sg',
       'Mn','Tc','Re','Bh','Fe','Ru','Os','Hs','Co','Rh','Ir','Mt','Ni','Pd','Pt','Ds','Cu','Ag','Au','Rg','Zn','Cd','Hg','B','Al','Ga','In','Tl','C','Si',
       'Ge','Sn','Pb','N','P','As','Sb','Bi','O','Se','Te','Po','F','Cl','Br','I','At','He','Ne','Ar','Kr','Xe','Rn'];
-
     },
 
     update: function () {
-      var i;
-      for (i=0; i < this.elementName.length; i++){
+      var i, x;
+      /*
+      for (i=0, x=this.elementName.length; i < x; i++){
         console.log(this.elementName[i]);
       }
-
+      */
+      this.bromo = new Elemento(this);
+      this.bromo.getType();
      
     },
-
+    
     onInputDown: function () {
       this.game.state.start('menu');
     }
